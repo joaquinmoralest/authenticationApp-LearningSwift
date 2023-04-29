bb9a818b2d30f0bd8b8260eb7fb0bda3574b499c
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-04-29 16:24:50 -0400

    Initial Commit

---
+++ authentication/authenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  authenticationApp.swift
+//  authentication
+//
+//  Created by Joaquin Yapp on 29-04-23.
+//
+
+import SwiftUI
+
+@main
+struct authenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ authenticationUITests/authenticationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  authenticationUITestsLaunchTests.swift
+//  authenticationUITests
+//
+//  Created by Joaquin Yapp on 29-04-23.
+//
+
+import XCTest
+
+final class authenticationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
